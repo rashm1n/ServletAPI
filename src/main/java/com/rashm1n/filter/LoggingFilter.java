c2a56b42ef02080d7415aaaa08e65293cd68e6e0
rashm1n <rashmin.ravindu@gmail.com>
2021-03-27 21:38:59 +0530

    Minor Changes

--- src/main/java/com/rashm1n/filter/LoggingFilter.java
+++ src/main/java/com/rashm1n/filter/LoggingFilter.java
@@ -1,13 +1,9 @@
 package com.rashm1n.filter;
 
-import com.rashm1n.model.Student;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -24,7 +20,6 @@ public class LoggingFilter implements Filter {
         try {
             Files.write(Paths.get("/home/rashmin/Documents/log1.txt"), logText.getBytes(), StandardOpenOption.APPEND);
         }catch (IOException e) {
-            //exception handling left as an exercise for the reader
         }
         filterChain.doFilter(servletRequest,servletResponse);
     }
